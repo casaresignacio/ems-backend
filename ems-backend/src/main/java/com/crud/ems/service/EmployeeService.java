1ca90bc4f54d0c76bc3c9c4866ffb0da09e40f2d
Ignacio <casaresignacio17@gmail.com>
2025-02-18 21:46:19 -0300

    Adding get all employees method

--- ems-backend/src/main/java/com/crud/ems/service/EmployeeService.java
+++ ems-backend/src/main/java/com/crud/ems/service/EmployeeService.java
@@ -2,10 +2,14 @@ package com.crud.ems.service;
 
 import com.crud.ems.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
 
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 
 }
